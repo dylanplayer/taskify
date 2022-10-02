531f75000cd6a60312dac10e143debdee93552cf
Dylan Player <dylanplayer@Dylans-MacBook-Pro-3.local>
2022-10-01 19:28:14 -0700

    Update ESLint

--- app/ui/hooks/index.ts
+++ app/ui/hooks/index.ts
@@ -2,4 +2,3 @@ export { default as useCreateTaskMutation } from './useCreateTaskMutation';
 export { default as useDeleteTaskMutation } from './useDeleteTaskMutation';
 export { default as useTasksQuery } from './useTasksQuery';
 export { default as useTestQuery } from './useTestQuery';
-export { default as useForceUpdate } from './useForceUpdate';
